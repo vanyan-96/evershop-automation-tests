45b770ccae108eb181eb5d0b12a0b54a47582993
Tom D <tom_deprez@hotmail.fr>
2024-09-03 05:01:02 +0200

    headless

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-search-engine-choice-screen")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
